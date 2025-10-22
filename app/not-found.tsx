a7f51b9b62d6bd169e487fcdd83183aa5db39f3a
RyanWez <ryanwez25@gmail.com>
2025-10-22 10:06:54 +0630

    Feat:12

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function RootNotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/dashboard/error/404');
+  }, [router]);
+
+  return null;
+}
